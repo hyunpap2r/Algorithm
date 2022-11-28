42fee639905799e1addd42d438815802d58d56c7
Lee HyeonJong <91259577+hyunpap2r@users.noreply.github.com>
2022-11-28 16:30:19 +0900

    소수의 합과 가장 작은 수의 소수

--- BaekJoon/2581.cpp
+++ BaekJoon/2581.cpp
@@ -4,30 +4,50 @@ using namespace std;
 
 int main()
 {
-    int n , m;
-    cin >> n >> m;
+	int n, m;
+	cin >> n >> m;
+	if (n > m)
+	{
+		cout << "-1";
+		return 0;
+	}
+	vector<int> a;
+	
+	for (int i = n; i <= m; i++)
+	{	
+		int cnt = 0;
+		for (int j = 1; j <= m; j++)
+		{
+			if (i%j == 0)
+			{
+				cnt++;
+				if (cnt > 2)
+				{
+					break;
+				}
+			}
+		}
+		if (cnt == 2)
+		{
+			a.push_back(i);
+		}
+	}
+	int sum = 0;
+	for (int k = 0; k < a.size(); k++)
+	{
+		sum += a[k];
+	}
+	
+	if (a.size() >= 1)
+	{
+		cout << sum << "\n";
+		cout << a[0] << "\n";
+	}
+	else
+	{
+		cout << "-1";
+	}
+	a.clear();
 
-    vector<int> a;
-
-    for (int i = n; i <= m; i++)
-    {
-        for (int j = 1; j <= m; j++)
-        {
-            if(i%j == 0)
-            {
-                a.push_back(j);
-            }
-        }
-    }
-    int sum = 0;
-    for (int k = 0 ; k < a.size(); k++)
-    {
-        sum += a[k];
-    }
-
-    cout << sum << "\n";
-    cout << a[0] << "\n";
-
-
-    return 0;
+	return 0;
 }
